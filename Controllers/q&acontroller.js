37fb93b222d9c151e03cee89d511b42c3e9b8550
Dhanam <parekhdhanam@gmail.com>
2020-07-05 16:00:40 +0530

    Edited Q&A api

--- Controllers/q&acontroller.js
+++ Controllers/q&acontroller.js
@@ -1,5 +1,5 @@
+const Doctor = require('../model/Doctor');
 const Question = require('../model/Question.js');
-
 //Post a question
 exports.post_question = async(req, res) =>{
 
@@ -29,12 +29,12 @@ exports.post_question = async(req, res) =>{
 exports.get_questions = async(req, res) =>{
     const type = req.body.type;
     if(type){
-        Question.find({regex:type}).sort({asked_by:-1}).exec()
+        Question.find({question:{$regex:type, $options:'i'}}).sort({asked_on:-1}).exec()
         .then(data => res.status(200).send(data))
         .catch(err => res.status(400).send('Error in getting questions ' + err));
     }
     else{
-        Question.find().sort({asked_by:-1}).exec()
+        Question.find().sort({asked_on:-1}).exec()
         .then(data => res.status(200).send(data))
         .catch(err => res.status(400).send('Error in getting questions ' + err));
    
@@ -42,7 +42,44 @@ exports.get_questions = async(req, res) =>{
 };
 
 //Post a answer
-exports.post_answer = async(req, res) => {
+exports.post_answer = async(req, res) =>{
+        const question = req.body.question;
+        const asked_by = req.body.asked_by;
+        const answer = req.body.answer;
+        const doctor_id = req.params.id;
+        
+        try{
+            const question_object = await Question.findOne({question:question, asked_by:asked_by});
+            question_id = question_object._id;
+            console.log(question_object.answers.length);
+            question_object['answers'].push(answer);
+            question_object['answered_by'].push(doctor_id)
+            question_object.save();
+            try{
+                let doctor = await Doctor.findOne({doctor_id:doctor_id});    
+                if(doctor){              
+                    doctor['answers'].push(question_id);
+                    await doctor.save();
+                    res.status(200).send('Added question_id to doctors list');
+                }
+                else{
+                    doctor = new Doctor({
+                        doctor_id: doctor_id,
+                        answers:[question_id]
+                    });
+                    doctor.save();
+                    res.status(200).send('New entry for doctor saved');
+                }
+            }
+            catch(err){
+                res.status(400).send('Error posting answer ' + err);
     
-
+            }
+    
+    
+        }
+        catch(err){
+            res.status(400).send('Error posting answer ' + err);
+        }
+    
 };
--- Routes/q&a.js
+++ Routes/q&a.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const QAController = require('../Controllers/q&acontroller');
-const Doctor = require('../model/Doctor');
-const Question = require('../model/Question.js');
+
 
 
 
@@ -12,48 +11,8 @@ router.post('/question/:id', QAController.post_question);
 
 //This route is used to post an answer to a question
 //Here ID corresponds to Question_id
-router.post('/answer/:id', async(req, res) =>{
-    const question = req.body.question;
-    const asked_by = req.body.asked_by;
-    const answer = req.body.answer;
-    const doctor_id = req.params.id;
-    
-    try{
-        const question_object = await Question.findOne({question:question, asked_by:asked_by});
-        question_id = question_object._id;
-        
-        question_object['answers'].push(answer);
-        question_object['answered_by'].push(doctor_id)
-        question_object.save();
-        try{
-            let doctor = await Doctor.findOne({doctor_id:doctor_id});
-            console.log('doctor is ' + doctor);
+router.post('/answer/:id', QAController.post_answer);
 
-            if(doctor){              
-                doctor['answers'].push(question_id);
-                await doctor.save();
-                res.status(200).send('Added question_id to doctors list');
-            }
-            else{
-                doctor = new Doctor({
-                    doctor_id: doctor_id,
-                    answers:[question_id]
-                });
-                doctor.save();
-                res.status(200).send('New entry for doctor saved');
-            }
-        }
-        catch(err){
-            res.status(400).send('Error posting answer ' + err);
-
-        }
-
-
-    }
-    catch(err){
-        res.status(400).send('Error posting answer ' + err);
-    }
-    
-});
+router.get('/questions', QAController.get_questions);
 
 module.exports = router;
