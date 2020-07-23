1229391599859cadff7395a628b8294f8c67be65
Dhanam <parekhdhanam@gmail.com>
2020-07-23 11:01:59 +0530

    Added env file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const passport = require('passport');
 
@@ -15,7 +16,7 @@ const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID,
 //const MongoClient = require('mongodb').MongoClient;   
 
 app.use(express.json());
-require('dotenv').config({ path: '.env' });
+dotenv.config();
 /*    
 
 //To Routes Folder
@@ -31,7 +32,7 @@ const searchRoute = require('./Routes/search');
 const qaRoute = require('./Routes/q&a');
 
 //Connecting to DB
-mongoose.connect('mongodb+srv://dbUser:kpnajdd@05@data-yetle.mongodb.net/test',
+mongoose.connect(process.env.DB_CONNECT,
     { useNewUrlParser: true },
     function (){
         console.log('connected to database');
